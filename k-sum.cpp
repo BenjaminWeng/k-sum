db612ebafc2b72a75d0c2687ba32c06e7d77f0ef
benjamin <yuu750110@gmail.com>
2018-08-24 09:54:52 +0800

    modification.

--- k-sum.cpp
+++ k-sum.cpp
@@ -7,47 +7,48 @@ using namespace std;
 class Solution {
 public:
     vector< vector<int> > fourSum(vector<int>& num, int target) {
-        vector< vector<int> > allSol;
         vector<int> sol;
+        vector< vector<int> > resSol;
         sort(num.begin(), num.end());
-        kSum(num, 0, num.size() - 1, target, 4, sol, allSol);
-        return allSol;
+        kSum(num, 0, num.size()-1, target, 4, sol, resSol);
+        return resSol;
     };
 
-    void kSum(vector<int> &num, int start, int end, int target, int k, vector<int> &sol, vector< vector<int> > &allSol) {
-        if (k == 2) {
-            twoSum(num, start, end, target, sol, allSol);
-            return;
-        }
-
-        for (int i = start; i<end; i++)
+    void kSum(vector<int>& num, int start, int end, int target, int k, vector<int>& sol, vector< vector<int> >& resSol)
+    {
+        if (k==2)
+            twoSum(num, start, end, target, sol, resSol);
+        else
         {
-            if (i>start && num[i] == num[i - 1]) continue;//i>start is very important!!
-            sol.push_back(num[i]);
-            kSum(num, i + 1, end, target - num[i], k - 1, sol, allSol);
-            sol.pop_back();
+            for (int i=start; i< end; i++)
+            {
+                if (i>start && num[i]==num[i-1]) continue;
+                sol.push_back(num[i]);
+                kSum(num, i+1, end, target-num[i], k-1, sol, resSol);
+                sol.pop_back();
+            }
         }
     };
-
-    void twoSum(vector<int> &num, int start, int end, int target, vector<int> &sol, vector< vector<int> > &allSol) {
-        while (start<end)
+    
+    void twoSum(vector<int>& num, int start, int end, int target,  vector<int>& sol, vector< vector<int> >& resSol)
+    {
+        while (start < end)
         {
-            int sum = num[start] + num[end];
-            if (sum == target) {
+            int sum = num[start]+num[end];
+            if (sum == target)
+            {
                 sol.push_back(num[start]);
                 sol.push_back(num[end]);
-                allSol.push_back(sol);
+                resSol.push_back(sol);
                 sol.pop_back();
                 sol.pop_back();
                 start++;
                 end--;
-                while (num[start] == num[start - 1]) start++;
-                while (num[end] == num[end + 1]) end--;
+                while (num[start]==num[start-1]) start++;
+                while (num[end]==num[end+1]) end--;
             }
-            else if (sum<target)
-                start++;
-            else
-                end--;
+            else if (sum < target) start++;
+            else end--;
         }
     };
 };
